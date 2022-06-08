5513bee090dc66ff5e6cd2b9c3a7f537224abea1
FLiindberg <70882168+FLiindberg@users.noreply.github.com>
2022-06-08 08:34:43 +0200

    Data Seed Fix

--- HandballAPI/Model/DataSeed.cs
+++ HandballAPI/Model/DataSeed.cs
@@ -7,8 +7,8 @@ namespace HandballAPI.Model
         public static void Seed(this ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Post>().HasData(
-                new Post { PostId = 1, Title = "Test 1", Description = "Test 11", Link = "Test", Author = "Filip Lindberg", Team = "P08"/*, UserId = 1*/ },
-                new Post { PostId = 2, Title = "Test 2", Description = "Test 22", Link = "Test 2", Author = "Filip Lindberg", Team = "P08"/*, UserId = 2*/ }
+                new Post { PostId = 1, Title = "Jugge", Description = "Övningen jugge...", Link = "Test", Author = "Filip Lindberg", Team = "Herrarna" },
+                new Post { PostId = 2, Title = "Utvisningspel", Description = "Utvisningsepelets tanke är att...", Link = "Test 2", Author = "Filip Lindberg", Team = "P08" }
                 );
         }
     }
